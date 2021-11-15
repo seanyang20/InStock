4d33b0d474fdef61b5b94bddbd58708e93a41ccd
Joshua Young <prgrssn@gmail.com>
2021-11-15 14:56:37 -0800

    client side browser router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/warehouses" component={null} />
+          <Route
+            path="/warehouses/:id"
+            render={(props) => {
+              return null;
+              // pass in browser-router-props into warehouse component
+            }}
+          />
+          <Route path="/inventory" component={null} />
+        </Switch>
+      </Router>
     </div>
   );
 }
