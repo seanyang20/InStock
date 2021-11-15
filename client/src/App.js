165a3159f5d254fe85d7c621d631f119ced2d323
Joshua Young <prgrssn@gmail.com>
2021-11-15 15:05:43 -0800

    created client side browser router routes and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Warehouses from "../pages/Warehouses/Warehouses";
+import Inventory from "../pages/Inventory/Inventory";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/warehouses" component={null} />
+          <Route path="/warehouses" component={Warehouses} />
           <Route
             path="/warehouses/:id"
             render={(props) => {
-              return null;
-              // pass in browser-router-props into warehouse component
+              return <Warehouses {...props} />;
             }}
           />
-          <Route path="/inventory" component={null} />
+          <Route path="/inventory" component={Inventory} />
         </Switch>
       </Router>
     </div>
