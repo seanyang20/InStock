36179ef9bf83f38b5b093afa1797b6a9d9127ea7
Hannah Park <hannahp056@gmail.com>
2021-11-16 17:51:09 -0500

    create tablet styles

--- client/src/pages/AddWarehouse/AddWarehouse.js
+++ client/src/pages/AddWarehouse/AddWarehouse.js
@@ -39,92 +39,97 @@ export default function AddWarehouse() {
         </div>
 
         <form onSubmit={handleSubmit} className="add-warehouse__form">
-          <section className="add-warehouse__form-section">
-            <h2 className="add-warehouse__section-title">Warehouse Details</h2>
-            <label className="add-warehouse__input-label" for="warehouseName">
-              Warehouse Name
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="warehouseName"
-              placeholder="
+          <div className="add-warehouse__flex-container">
+            <section className="add-warehouse__form-section">
+              <h2 className="add-warehouse__section-title">
+                Warehouse Details
+              </h2>
+              <label className="add-warehouse__input-label" for="warehouseName">
+                Warehouse Name
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="warehouseName"
+                placeholder="
       Warehouse Name"
-            ></input>
-            <label className="add-warehouse__input-label" for="streetAdress">
-              Street Address
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="streetAddress"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="streetAdress">
+                Street Address
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="streetAddress"
+                placeholder="
       Street Address"
-            ></input>
-            <label className="add-warehouse__input-label" for="city">
-              City
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="city"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="city">
+                City
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="city"
+                placeholder="
       City"
-            ></input>
-            <label className="add-warehouse__input-label" for="country">
-              Country
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="country"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="country">
+                Country
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="country"
+                placeholder="
       Country"
-            ></input>
-          </section>
-          <section className="add-warehouse__form-section">
-            <h2 className="add-warehouse__section-title">Contact Details</h2>
-            <label className="add-warehouse__input-label" for="contactName">
-              Contact Name
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="contactName"
-              placeholder="
+              ></input>
+            </section>
+            <section className="add-warehouse__form-section">
+              <h2 className="add-warehouse__section-title">Contact Details</h2>
+              <label className="add-warehouse__input-label" for="contactName">
+                Contact Name
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="contactName"
+                placeholder="
       Contact Name"
-            ></input>
-            <label className="add-warehouse__input-label" for="position">
-              Position
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="position"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="position">
+                Position
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="position"
+                placeholder="
       Position"
-            ></input>
-            <label className="add-warehouse__input-label" for="phoneNumber">
-              Phone Number
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="phoneNumber"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="phoneNumber">
+                Phone Number
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="phoneNumber"
+                placeholder="
       Phone Number"
-            ></input>
-            <label className="add-warehouse__input-label" for="email">
-              Email
-            </label>
-            <input
-              className="add-warehouse__input"
-              type="text"
-              name="email"
-              placeholder="
+              ></input>
+              <label className="add-warehouse__input-label" for="email">
+                Email
+              </label>
+              <input
+                className="add-warehouse__input"
+                type="text"
+                name="email"
+                placeholder="
       Email"
-            ></input>
-          </section>
+              ></input>
+            </section>
+          </div>
+
           <section className="add-warehouse__buttons">
             <button className="add-warehouse__cancel-button">Cancel</button>
             <button type="submit" className="add-warehouse__add-button">
