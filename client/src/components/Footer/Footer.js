d058c599788dd613803622823ef5efd775887e4d
Joshua Young <prgrssn@gmail.com>
2021-11-15 18:26:03 -0800

    create footer component and add the component folder into client/src. Link the styles page to footer component.

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <img className="footer__copyright" alt="InStock Copy Right Logo" />
+    </footer>
+  );
+}
