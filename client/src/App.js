5ad9b77b02423efb74339d746cb2261840423c72
Joshua Young <prgrssn@gmail.com>
2021-11-16 14:27:23 -0800

    add style to the inventory list header section

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Warehouses from "./pages/Warehouses/Warehouses";
 import Inventory from "./pages/Inventory/Inventory";
+import InventoryList from "./components/InventoryList/InventoryList";
 
 function App() {
   return (
     <div className="App">
+      <InventoryList />
       <Router>
         {/* <Header/> */}
         <Switch>
--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -1,3 +1,22 @@
 import "./InventoryList.scss";
 
-export default function InventoryList() {}
+export default function InventoryList() {
+  return (
+    <section className="inventory-list">
+      <article className="inventory-list__head">
+        <h1 className="inventory-list__head--title">Inventory</h1>
+        <form className="inventory-list__form">
+          <input
+            name="search"
+            placeholder="Search..."
+            className="inventory-list__form--input"
+          />
+          <button className="inventory-list__form--button">Add New Item</button>
+        </form>
+      </article>
+      <article>
+        {/* {items.map((item) => (<ItemCard key={item.id} item={item}))} */}
+      </article>
+    </section>
+  );
+}
