5becf4544644efaa2ce5025a5dd809513fe1d674
Hannah Park <hannahp056@gmail.com>
2021-11-16 16:39:17 -0500

    add form functionality

--- client/src/pages/AddWarehouse/AddWarehouse.js
+++ client/src/pages/AddWarehouse/AddWarehouse.js
@@ -1,11 +1,39 @@
 import "./AddWarehouse.scss";
+import axios from "axios";
+const apiUrl = "http://localhost:8080";
 
 export default function AddWarehouse() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newWarehouse = {
+      name: event.target.warehouseName.value,
+      address: event.target.streetAddress.value,
+      city: event.target.city.value,
+      country: event.target.country.value,
+      contact: {
+        name: event.target.contactName.value,
+        position: event.target.position.value,
+        phone: event.target.phoneNumber.value,
+        email: event.target.email.value,
+      },
+    };
+    console.log(newWarehouse);
+    axios
+      .post(`${apiUrl}`, newWarehouse)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <article className="temp-background">
       <section className="add-warehouse">
         <h1 className="add-warehouse__title">Add New Warehouse</h1>
-        <form className="add-warehouse__form">
+        <form onSubmit={handleSubmit} className="add-warehouse__form">
           <section className="add-warehouse__form-section">
             <h2 className="add-warehouse__section-title">Warehouse Details</h2>
             <label className="add-warehouse__input-label" for="warehouseName">
@@ -13,7 +41,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="warehouseName"
+              type="text"
+              name="warehouseName"
               placeholder="
       Warehouse Name"
             ></input>
@@ -22,7 +51,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="streetAddress"
+              type="text"
+              name="streetAddress"
               placeholder="
       Street Address"
             ></input>
@@ -31,7 +61,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="city"
+              type="text"
+              name="city"
               placeholder="
       City"
             ></input>
@@ -40,7 +71,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="country"
+              type="text"
+              name="country"
               placeholder="
       Country"
             ></input>
@@ -52,7 +84,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="contactName"
+              type="text"
+              name="contactName"
               placeholder="
       Contact Name"
             ></input>
@@ -61,7 +94,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="position"
+              type="text"
+              name="position"
               placeholder="
       Position"
             ></input>
@@ -70,7 +104,8 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="phoneNumber"
+              type="text"
+              name="phoneNumber"
               placeholder="
       PhoneNumber"
             ></input>
@@ -79,14 +114,17 @@ export default function AddWarehouse() {
             </label>
             <input
               className="add-warehouse__input"
-              id="email"
+              type="text"
+              name="email"
               placeholder="
       Email"
             ></input>
           </section>
           <section className="add-warehouse__buttons">
-          <button className="add-warehouse__cancel-button">Cancel</button>
-          <button className="add-warehouse__add-button">+ Add Warehouse</button>
+            <button className="add-warehouse__cancel-button">Cancel</button>
+            <button type="submit" className="add-warehouse__add-button">
+              + Add Warehouse
+            </button>
           </section>
         </form>
       </section>
