208b3e9fa78393032c19fce78f4ef064e11726cf
Hannah Park <hannahp056@gmail.com>
2021-11-16 10:17:09 -0500

    create bem class names

--- client/src/components/AddWarehouse/AddWarehouse.js
+++ client/src/components/AddWarehouse/AddWarehouse.js
@@ -2,65 +2,91 @@ import "./AddWarehouse.scss";
 
 export default function AddWarehouse() {
   return (
-    <section>
-      <h1>Add New Warehouse</h1>
-      <form>
-        <section>
-          <h2>Warehouse Details</h2>
-          <label for="warehouseName">Warehouse Name</label>
+    <section className="add-warehouse">
+      <h1 className="add-warehouse__title">Add New Warehouse</h1>
+      <form className="add-warehouse__form">
+        <section className="add-warehouse__form-section">
+          <h2 className="add-warehouse__section-title">Warehouse Details</h2>
+          <label className="add-warehouse__input-label" for="warehouseName">
+            Warehouse Name
+          </label>
           <input
+            className="add-warehouse__input"
             id="warehouseName"
             placeholder="
       Warehouse Name"
           ></input>
-          <label for="streetAdress">Street Address</label>
+          <label className="add-warehouse__input-label" for="streetAdress">
+            Street Address
+          </label>
           <input
+            className="add-warehouse__input"
             id="streetAddress"
             placeholder="
       Street Address"
           ></input>
-          <label for="city">City</label>
+          <label className="add-warehouse__input-label" for="city">
+            City
+          </label>
           <input
+            className="add-warehouse__input"
             id="city"
             placeholder="
       City"
           ></input>
-          <label for="country">Country</label>
+          <label className="add-warehouse__input-label" for="country">
+            Country
+          </label>
           <input
+            className="add-warehouse__input"
             id="country"
             placeholder="
       Country"
           ></input>
         </section>
-        <section>
-          <h2>Contact Details</h2>
-          <label for="contactName">Contact Name</label>
+        <section className="add-warehouse__form-section">
+          <h2 className="add-warehouse__section-title">Contact Details</h2>
+          <label className="add-warehouse__input-label" for="contactName">
+            Contact Name
+          </label>
           <input
+            className="add-warehouse__input"
             id="contactName"
             placeholder="
       Contact Name"
           ></input>
-          <label for="position">Position</label>
+          <label className="add-warehouse__input-label" for="position">
+            Position
+          </label>
           <input
+            className="add-warehouse__input"
             id="position"
             placeholder="
       Position"
           ></input>
-          <label for="phoneNumber">Phone Number</label>
+          <label className="add-warehouse__input-label" for="phoneNumber">
+            Phone Number
+          </label>
           <input
+            className="add-warehouse__input"
             id="phoneNumber"
             placeholder="
       PhoneNumber"
           ></input>
-          <label for="email">Email</label>
+          <label className="add-warehouse__input-label" for="email">
+            Email
+          </label>
           <input
+            className="add-warehouse__input"
             id="email"
             placeholder="
       Email"
           ></input>
         </section>
-        <button>Cancel</button>
-        <button>+ Add Warehouse</button>
+        <button className="add-warehouse__cancel-button">Cancel</button>
+        <button className="add-warehouse__add-button">
+          + Add Warehouse
+        </button>
       </form>
     </section>
   );
