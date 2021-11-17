6a24c63e84f24e72f7275ee9be3600e3dac69136
Sean Yang <seanyixuan@gmail.com>
2021-11-17 14:08:00 -0800

    resolved cannot set headers error for put route

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -22,6 +22,51 @@ router.get("/inventories", (req, res) => {
   res.json(inventoryData);
 });
 
+router.put("/inventories/:inventoryId", (req, res) => {
+  console.log("Inside ROUTER PUT to edit an inventory item");
+  const inventoryId = req.params.inventoryId
+  // console.log(inventoryId);           // requested inventory id
+  const selectedInventory = inventoryData.find(
+      (inventory) => 
+      // console.log(video, "TESTING")
+      inventory.id === inventoryId
+      )
+  // console.log(selectedInventory);      // returned selected inventory
+// To test in Postman
+// {
+//   "itemName": "Television",
+//   "warehouseName": "Manhattan",
+//   "description": "This 50, 4K LED TV provides a crystal-clear picture and vivid colors.",
+//   "category": "Electronics",
+//   "status": "In Stock"
+// }
+             
+// to enable editing of these properties on the front-end
+selectedInventory.itemName = req.body.itemName;
+selectedInventory.warehouseName = req.body.warehouseName;
+selectedInventory.description = req.body.description;
+selectedInventory.category = req.body.category;
+selectedInventory.status = req.body.status;
+// to replace the old inventory item with the edited version in the array
+  inventoryData.map((invInArray) => {
+      if (invInArray.id === selectedInventory.id){
+          return invInArray = selectedInventory
+      }
+  })
+  // console.log(inventoryData);;
+  // to send edited data to endpoint
+  // res.json(inventoryData);
+  // to update the inventories.json file 
+  fs.writeFile('./data/inventories.json', 
+  JSON.stringify(inventoryData), (err) => {
+      if (err) {
+          res.status(500).send(err);
+      }
+      console.log("File written successfully!");
+      res.status(201).json(inventoryData);
+  })
+})
+
 router.get("/warehouses/:warehouseId/inventories", (req, res) => {
   console.log("Inside Router /GET for getting inventories for given warehouse");
 
