866316ab90798abb1ad87df3ad752c49bfaf3df4
Hannah Park <hannahp056@gmail.com>
2021-11-17 18:32:02 -0500

    use location.pathname to conditionally add current class to buttons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,39 @@
 import "./Header.scss";
 import logo from "../../assets/Logo/InStock-Logo_2x.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+
+  const handleClick = () => {
+    console.log(location.pathname);
+  };
+
   return (
     <section className="header">
       <img src={logo} alt="Instock logo" className="header__logo" />
       <div className="header__buttons-container">
         <Link to="/warehouses">
-          <button className="header__button">Warehouses</button>
+          <button
+            onClick={handleClick}
+            className={`header__button ${
+              location.pathname === "/warehouses"
+                ? "header__button--current"
+                : ""
+            }`}
+          >
+            Warehouses
+          </button>
         </Link>
-        <Link to="inventories">
-          <button className="header__button header__button--current">
+        <Link to="/inventories">
+          <button
+            onClick={handleClick}
+            className={`header__button ${
+              location.pathname === "/inventories"
+                ? "header__button--current"
+                : ""
+            }`}
+          >
             Inventory
           </button>
         </Link>
