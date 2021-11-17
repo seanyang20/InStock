8dadbdb283e4f568401d3f21060002f5cd999f5a
Joshua Young <prgrssn@gmail.com>
2021-11-16 20:08:01 -0800

    work on layout for item cards in mobile view to resemble mockup

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -6,22 +6,32 @@ export default function ItemCard({ item }) {
   console.log(item);
   return (
     <article className="item-card">
-      <div className="item-card__title-cont">
-        <h6 className="item-card__subhead">INVENTORY ITEM</h6>
-        <p className="item-card__value">{item.itemName}</p>
-      </div>
-      <div className="item-card__cat-cont">
-        <h6 className="item-card__subhead">CATEGORY</h6>
-        <p className="item-card__value">{item.category}</p>
-      </div>
-      <div className="item-card__stat-cont">
-        <h6 className="item-card__subhead">STATUS</h6>
-        <p className="item-card__value">{item.status}</p>
-      </div>
-      <div className="item-card__qty-cont">
-        <h6 className="item-card__subhead">QTY</h6>
-        <p className="item-card__value">{item.quantity}</p>
-      </div>
+      <section className="item-card__info-section">
+        <div className="item-card__info-section--left">
+          <div className="item-card__title-cont">
+            <h6 className="item-card__subhead">INVENTORY ITEM</h6>
+            <p className="item-card__value">{item.itemName}</p>
+          </div>
+          <div className="item-card__cat-cont">
+            <h6 className="item-card__subhead">CATEGORY</h6>
+            <p className="item-card__value">{item.category}</p>
+          </div>
+        </div>
+        <div className="item-card__info-section--right">
+          <div className="item-card__stat-cont">
+            <h6 className="item-card__subhead">STATUS</h6>
+            <p className="item-card__value">{item.status}</p>
+          </div>
+          <div className="item-card__qty-cont">
+            <h6 className="item-card__subhead">QTY</h6>
+            <p className="item-card__value">{item.quantity}</p>
+          </div>
+          <div className="item-card__loc-cont">
+            <h6 className="item-card__subhead">WAREHOUSE</h6>
+            <p className="item-card__value">{item.warehouseName}</p>
+          </div>
+        </div>
+      </section>
       <section className="item-card__chg-cont">
         <div className="item-card__delete-cont">
           <img
--- client/src/pages/Inventory/Inventory.js
+++ client/src/pages/Inventory/Inventory.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import InventoryList from "../../components/InventoryList/InventoryList";
 
+const inventory_API_URL = "http://localhost:8080/inventories";
+
 export default function Inventory() {
   const [inventoryList, setInventoryList] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/inventories").then((res) => {
+    axios.get(inventory_API_URL).then((res) => {
       setInventoryList(res.data);
     });
   }, []);
