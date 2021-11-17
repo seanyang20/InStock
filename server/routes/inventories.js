df65b95f622ca20b41067a123e15b9a3fd8e50b4
Sean Yang <seanyixuan@gmail.com>
2021-11-17 14:43:46 -0800

    added form validation and re-added router get for a single inventory item that was accidentally removed in the previous commit

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -22,6 +22,22 @@ router.get("/inventories", (req, res) => {
   res.json(inventoryData);
 });
 
+router.get("/inventories/:inventoryId", (req, res) => {
+  console.log("Inside Router /Get for a single inventory item");
+
+  console.log(req.params.inventoryId);
+  const singleInventory = inventoryData.find((inventory) => {
+    return inventory.id === req.params.inventoryId;
+})
+// console.log(singleInventory);
+if (singleInventory) {
+    res.json(singleInventory);
+} else {
+    res.status(404).send("We can't find that inventory item.");
+}
+
+});
+
 router.put("/inventories/:inventoryId", (req, res) => {
   console.log("Inside ROUTER PUT to edit an inventory item");
   const inventoryId = req.params.inventoryId
@@ -32,6 +48,7 @@ router.put("/inventories/:inventoryId", (req, res) => {
       inventory.id === inventoryId
       )
   // console.log(selectedInventory);      // returned selected inventory
+
 // To test in Postman
 // {
 //   "itemName": "Television",
@@ -40,13 +57,42 @@ router.put("/inventories/:inventoryId", (req, res) => {
 //   "category": "Electronics",
 //   "status": "In Stock"
 // }
-             
+            
+const { itemName, warehouseName, description, category, status } =
+req.body;
+
 // to enable editing of these properties on the front-end
-selectedInventory.itemName = req.body.itemName;
-selectedInventory.warehouseName = req.body.warehouseName;
-selectedInventory.description = req.body.description;
-selectedInventory.category = req.body.category;
-selectedInventory.status = req.body.status;
+const itemNameIsValid = itemName.length > 0;
+const warehouseNameIsValid = warehouseName.length > 0;
+const descriptionIsValid = description.length > 0;
+const categoryIsValid = category.length > 0;
+const statusIsValid = status.length > 0;
+
+// to enable editing of these properties on the front-end
+// selectedInventory.itemName = req.body.itemName;
+// selectedInventory.warehouseName = req.body.warehouseName;
+// selectedInventory.description = req.body.description;
+// selectedInventory.category = req.body.category;
+// selectedInventory.status = req.body.status;
+if (
+  !itemNameIsValid ||
+  !warehouseNameIsValid ||
+  !descriptionIsValid ||
+  !categoryIsValid||
+  !statusIsValid
+) {
+  res
+    .status(400)
+    .send("One or more form input values is missing or invalid.");
+}
+
+// to enable editing of these properties on the front-end
+selectedInventory.itemName = itemName;
+selectedInventory.warehouseName = warehouseName;
+selectedInventory.description = description;
+selectedInventory.category = category;
+selectedInventory.status = status;
+
 // to replace the old inventory item with the edited version in the array
   inventoryData.map((invInArray) => {
       if (invInArray.id === selectedInventory.id){
