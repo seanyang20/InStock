c275d3e359f2fc6d4c959f421d013b9b8b5cfed8
Hannah Park <hannahp056@gmail.com>
2021-11-16 23:06:11 -0500

    create form validation with useState

--- client/src/pages/AddWarehouse/AddWarehouse.js
+++ client/src/pages/AddWarehouse/AddWarehouse.js
@@ -1,24 +1,85 @@
 import "./AddWarehouse.scss";
 import axios from "axios";
 import arrow from "../../assets/Icons/arrow_back-24px.svg";
+import error from "../../assets/Icons/error-24px.svg";
+import { useState } from "react";
 const apiUrl = "http://localhost:8080";
 
 export default function AddWarehouse() {
+  const [validation, setValidation] = useState({
+    name: true,
+    address: true,
+    city: true,
+    country: true,
+    contactName: true,
+    position: true,
+    phone: true,
+    email: true,
+  });
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const {
+      warehouseName,
+      streetAddress,
+      city,
+      country,
+      contactName,
+      position,
+      phoneNumber,
+      email,
+    } = event.target;
+
+    const nameIsValid = warehouseName.value.length > 0;
+    const addressIsValid = streetAddress.value.length > 0;
+    const cityIsValid = city.value.length > 0;
+    const countryIsValid = country.value.length > 0;
+    const contactNameIsValid = contactName.value.length > 0;
+    const positionIsValid = position.value.length > 0;
+    const phoneIsValid = !!phoneNumber.value.match(
+      /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im
+    );
+    const emailIsValid = !!email.value.match(
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    );
+
+    setValidation({
+      name: nameIsValid,
+      address: addressIsValid,
+      city: cityIsValid,
+      country: countryIsValid,
+      contactName: contactNameIsValid,
+      position: positionIsValid,
+      phone: phoneIsValid,
+      email: emailIsValid,
+    });
+
+    if (
+      !nameIsValid ||
+      !addressIsValid ||
+      !cityIsValid ||
+      !countryIsValid ||
+      !contactNameIsValid ||
+      !positionIsValid ||
+      !phoneIsValid ||
+      !emailIsValid
+    ) {
+      return;
+    }
 
     const newWarehouse = {
-      name: event.target.warehouseName.value,
-      address: event.target.streetAddress.value,
-      city: event.target.city.value,
-      country: event.target.country.value,
+      name: warehouseName.value,
+      address: streetAddress.value,
+      city: city.value,
+      country: country.value,
       contact: {
-        name: event.target.contactName.value,
-        position: event.target.position.value,
-        phone: event.target.phoneNumber.value,
-        email: event.target.email.value,
+        name: contactName.value,
+        position: position.value,
+        phone: phoneNumber.value,
+        email: email.value,
       },
     };
+
     console.log(newWarehouse);
     axios
       .post(`${apiUrl}`, newWarehouse)
@@ -29,6 +90,7 @@ export default function AddWarehouse() {
         console.log(error);
       });
   };
+  console.log(validation);
 
   return (
     <article className="temp-background">
@@ -44,92 +106,170 @@ export default function AddWarehouse() {
               <h2 className="add-warehouse__section-title">
                 Warehouse Details
               </h2>
-              <label className="add-warehouse__input-label" for="warehouseName">
-                Warehouse Name
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="warehouseName"
-                placeholder="
+              <div className="add-warehouse__form-field">
+                <label
+                  className="add-warehouse__input-label"
+                  htmlFor="warehouseName"
+                >
+                  Warehouse Name
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="warehouseName"
+                  placeholder="
       Warehouse Name"
-              ></input>
-              <label className="add-warehouse__input-label" for="streetAdress">
-                Street Address
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="streetAddress"
-                placeholder="
+                ></input>
+                {!validation.name && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label
+                  className="add-warehouse__input-label"
+                  htmlFor="streetAdress"
+                >
+                  Street Address
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="streetAddress"
+                  placeholder="
       Street Address"
-              ></input>
-              <label className="add-warehouse__input-label" for="city">
-                City
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="city"
-                placeholder="
+                ></input>
+                {!validation.address && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label className="add-warehouse__input-label" htmlFor="city">
+                  City
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="city"
+                  placeholder="
       City"
-              ></input>
-              <label className="add-warehouse__input-label" for="country">
-                Country
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="country"
-                placeholder="
+                ></input>
+                {!validation.city && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label className="add-warehouse__input-label" htmlFor="country">
+                  Country
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="country"
+                  placeholder="
       Country"
-              ></input>
+                ></input>
+                {!validation.country && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
             </section>
             <section className="add-warehouse__form-section">
               <h2 className="add-warehouse__section-title">Contact Details</h2>
-              <label className="add-warehouse__input-label" for="contactName">
-                Contact Name
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="contactName"
-                placeholder="
+              <div className="add-warehouse__form-field">
+                <label
+                  className="add-warehouse__input-label"
+                  htmlFor="contactName"
+                >
+                  Contact Name
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="contactName"
+                  placeholder="
       Contact Name"
-              ></input>
-              <label className="add-warehouse__input-label" for="position">
-                Position
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="position"
-                placeholder="
+                ></input>
+                {!validation.contactName && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label
+                  className="add-warehouse__input-label"
+                  htmlFor="position"
+                >
+                  Position
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="position"
+                  placeholder="
       Position"
-              ></input>
-              <label className="add-warehouse__input-label" for="phoneNumber">
-                Phone Number
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="phoneNumber"
-                placeholder="
+                ></input>
+                {!validation.position && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label
+                  className="add-warehouse__input-label"
+                  htmlFor="phoneNumber"
+                >
+                  Phone Number
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="
       Phone Number"
-              ></input>
-              <label className="add-warehouse__input-label" for="email">
-                Email
-              </label>
-              <input
-                className="add-warehouse__input"
-                type="text"
-                name="email"
-                placeholder="
+                ></input>
+                {!validation.phone && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
+              <div className="add-warehouse__form-field">
+                <label className="add-warehouse__input-label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="add-warehouse__input"
+                  type="text"
+                  name="email"
+                  placeholder="
       Email"
-              ></input>
+                ></input>
+                {!validation.email && (
+                  <div className="add-warehouse__input-error">
+                    <img src={error} alt="error icon" />
+                    <span>This field is required</span>
+                  </div>
+                )}
+              </div>
             </section>
           </div>
-
           <section className="add-warehouse__buttons">
             <button className="add-warehouse__cancel-button">Cancel</button>
             <button type="submit" className="add-warehouse__add-button">
