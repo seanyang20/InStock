e2770d45579899e9e3bafb5fa0b26f8573dc0b1c
Hannah Park <hannahp056@gmail.com>
2021-11-18 08:48:20 -0500

    tweak responsive styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,31 +11,33 @@ export default function Header() {
 
   return (
     <section className="header">
-      <img src={logo} alt="Instock logo" className="header__logo" />
-      <div className="header__buttons-container">
-        <Link to="/warehouses">
-          <button
-            onClick={handleClick}
-            className={`header__button ${
-              location.pathname.startsWith("/warehouses") &&
-              "header__button--current"
-            }`}
-          >
-            Warehouses
-          </button>
-        </Link>
-        <Link to="/inventories">
-          <button
-            onClick={handleClick}
-            className={`header__button ${
-              location.pathname.startsWith("/inventories") &&
-              "header__button--current"
-            }`}
-          >
-            Inventory
-          </button>
-        </Link>
-      </div>
+      <nav className="header__nav">
+        <img src={logo} alt="Instock logo" className="header__logo" />
+        <div className="header__buttons-container">
+          <Link to="/warehouses">
+            <button
+              onClick={handleClick}
+              className={`header__button ${
+                location.pathname.startsWith("/warehouses") &&
+                "header__button--current"
+              }`}
+            >
+              Warehouses
+            </button>
+          </Link>
+          <Link to="/inventories">
+            <button
+              onClick={handleClick}
+              className={`header__button ${
+                location.pathname.startsWith("/inventories") &&
+                "header__button--current"
+              }`}
+            >
+              Inventory
+            </button>
+          </Link>
+        </div>
+      </nav>
     </section>
   );
 }
