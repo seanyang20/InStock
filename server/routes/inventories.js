d5379c046cc278755961cadb7cc7272e0701b7e4
Hannah Park <hannahp056@gmail.com>
2021-11-18 11:33:53 -0500

    create endpoint that returns a new inventory object from request

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const uuid = require("uuid");
 
 // Read Data from JSON
 let inventoryData = [];
@@ -28,91 +29,91 @@ router.get("/inventories/:inventoryId", (req, res) => {
   console.log(req.params.inventoryId);
   const singleInventory = inventoryData.find((inventory) => {
     return inventory.id === req.params.inventoryId;
-})
-// console.log(singleInventory);
-if (singleInventory) {
+  });
+  // console.log(singleInventory);
+  if (singleInventory) {
     res.json(singleInventory);
-} else {
+  } else {
     res.status(404).send("We can't find that inventory item.");
-}
-
+  }
 });
-
 
 router.put("/inventories/:inventoryId", (req, res) => {
   console.log("Inside ROUTER PUT to edit an inventory item");
-  const inventoryId = req.params.inventoryId
+  const inventoryId = req.params.inventoryId;
   // console.log(inventoryId);           // requested inventory id
   const selectedInventory = inventoryData.find(
-      (inventory) => 
+    (inventory) =>
       // console.log(video, "TESTING")
       inventory.id === inventoryId
-      )
+  );
   // console.log(selectedInventory);      // returned selected inventory
 
-// To test in Postman
-// {
-//   "itemName": "Television",
-//   "warehouseName": "Manhattan",
-//   "description": "This 50, 4K LED TV provides a crystal-clear picture and vivid colors.",
-//   "category": "Electronics",
-//   "status": "In Stock"
-// }
-            
-const { itemName, warehouseName, description, category, status } =
-req.body;
+  // To test in Postman
+  // {
+  //   "itemName": "Television",
+  //   "warehouseName": "Manhattan",
+  //   "description": "This 50, 4K LED TV provides a crystal-clear picture and vivid colors.",
+  //   "category": "Electronics",
+  //   "status": "In Stock"
+  // }
 
-// to enable editing of these properties on the front-end
-const itemNameIsValid = itemName.length > 0;
-const warehouseNameIsValid = warehouseName.length > 0;
-const descriptionIsValid = description.length > 0;
-const categoryIsValid = category.length > 0;
-const statusIsValid = status.length > 0;
+  const { itemName, warehouseName, description, category, status } = req.body;
 
-// to enable editing of these properties on the front-end
-// selectedInventory.itemName = req.body.itemName;
-// selectedInventory.warehouseName = req.body.warehouseName;
-// selectedInventory.description = req.body.description;
-// selectedInventory.category = req.body.category;
-// selectedInventory.status = req.body.status;
-if (
-  !itemNameIsValid ||
-  !warehouseNameIsValid ||
-  !descriptionIsValid ||
-  !categoryIsValid||
-  !statusIsValid
-) {
-  res
-    .status(400)
-    .send("One or more form input values is missing or invalid.");
-}
+  // to enable editing of these properties on the front-end
+  const itemNameIsValid = itemName.length > 0;
+  const warehouseNameIsValid = warehouseName.length > 0;
+  const descriptionIsValid = description.length > 0;
+  const categoryIsValid = category.length > 0;
+  const statusIsValid = status.length > 0;
 
-// to enable editing of these properties on the front-end
-selectedInventory.itemName = itemName;
-selectedInventory.warehouseName = warehouseName;
-selectedInventory.description = description;
-selectedInventory.category = category;
-selectedInventory.status = status;
+  // to enable editing of these properties on the front-end
+  // selectedInventory.itemName = req.body.itemName;
+  // selectedInventory.warehouseName = req.body.warehouseName;
+  // selectedInventory.description = req.body.description;
+  // selectedInventory.category = req.body.category;
+  // selectedInventory.status = req.body.status;
+  if (
+    !itemNameIsValid ||
+    !warehouseNameIsValid ||
+    !descriptionIsValid ||
+    !categoryIsValid ||
+    !statusIsValid
+  ) {
+    res
+      .status(400)
+      .send("One or more form input values is missing or invalid.");
+  }
 
-// to replace the old inventory item with the edited version in the array
+  // to enable editing of these properties on the front-end
+  selectedInventory.itemName = itemName;
+  selectedInventory.warehouseName = warehouseName;
+  selectedInventory.description = description;
+  selectedInventory.category = category;
+  selectedInventory.status = status;
+
+  // to replace the old inventory item with the edited version in the array
   inventoryData.map((invInArray) => {
-      if (invInArray.id === selectedInventory.id){
-          return invInArray = selectedInventory
-      }
-  })
+    if (invInArray.id === selectedInventory.id) {
+      return (invInArray = selectedInventory);
+    }
+  });
   // console.log(inventoryData);;
   // to send edited data to endpoint
   // res.json(inventoryData);
-  // to update the inventories.json file 
-  fs.writeFile('./data/inventories.json', 
-  JSON.stringify(inventoryData), (err) => {
+  // to update the inventories.json file
+  fs.writeFile(
+    "./data/inventories.json",
+    JSON.stringify(inventoryData),
+    (err) => {
       if (err) {
-          res.status(500).send(err);
+        res.status(500).send(err);
       }
       console.log("File written successfully!");
       res.status(201).json(inventoryData);
-  })
-})
+    }
+  );
+});
 
 router.get("/warehouses/:warehouseId/inventories", (req, res) => {
   console.log("Inside Router /GET for getting inventories for given warehouse");
@@ -127,6 +128,46 @@ router.get("/warehouses/:warehouseId/inventories", (req, res) => {
   );
   console.log(warehouseInv);
   // res.json(inventoriesData);
+});
+
+// post request for adding new inventory item
+router.post("/inventories/add", (req, res) => {
+  const { itemName, warehouseName, description, category, status, quantity } =
+    req.body;
+  if (
+    !itemName ||
+    !warehouseName ||
+    !description ||
+    !category ||
+    !status ||
+    !quantity
+  ) {
+    res
+      .status(400)
+      .send("One or more form input values is missing or invalid.");
+  }
+
+  //find warehouse ID from the warehouse name input
+  const findWarehouse = inventoryData.find(
+    (inventory) => inventory.warehouseName === warehouseName
+  );
+  const warehouseID = findWarehouse.warehouseID;
+
+  const newInventory = {
+    id: uuid.v4(),
+    warehouseID: warehouseID,
+    warehouseName: warehouseName,
+    itemName: itemName,
+    description: description,
+    category: category,
+    status: status,
+    quantity: quantity,
+  };
+
+  // inventoryData.push(newInventory);
+  // writeinventoryData(inventoryData);
+
+  res.json(newInventory);
 });
 
 // module.exports = router;
