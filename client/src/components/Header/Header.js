4adb8764ab0ca678da2435c564b8b6780f907490
Hannah Park <hannahp056@gmail.com>
2021-11-18 08:49:33 -0500

    delete unecessary click handler

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,10 +5,6 @@ import { Link, useLocation } from "react-router-dom";
 export default function Header() {
   const location = useLocation();
 
-  const handleClick = () => {
-    console.log(location.pathname);
-  };
-
   return (
     <section className="header">
       <nav className="header__nav">
@@ -16,7 +12,6 @@ export default function Header() {
         <div className="header__buttons-container">
           <Link to="/warehouses">
             <button
-              onClick={handleClick}
               className={`header__button ${
                 location.pathname.startsWith("/warehouses") &&
                 "header__button--current"
@@ -27,7 +22,6 @@ export default function Header() {
           </Link>
           <Link to="/inventories">
             <button
-              onClick={handleClick}
               className={`header__button ${
                 location.pathname.startsWith("/inventories") &&
                 "header__button--current"
