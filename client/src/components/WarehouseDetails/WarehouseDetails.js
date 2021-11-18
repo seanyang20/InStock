50aee949fa1f59e005132192149096c2909df1a1
Joshua Young <prgrssn@gmail.com>
2021-11-17 16:03:14 -0800

    succes styling mobile view of the warehouse detail section in the warehousedetails page. Work on tablet view underway.

--- client/src/components/WarehouseDetails/WarehouseDetails.js
+++ client/src/components/WarehouseDetails/WarehouseDetails.js
@@ -7,11 +7,29 @@ export default function WarehouseDetails() {
     <section className="warehouse-details">
       <article className="head">
         <div className="head__back">
-          <img className="head__back--icon" alt="back icon" />
+          <img className="head__back--icon" src={backIcon} alt="back icon" />
         </div>
         <h1 className="head__title">King West</h1>
         <div className="head__edit">
-          <img className="head__edit--icon" alt="edit icon" />
+          <img className="head__edit--icon" src={editIcon} alt="edit icon" />
+        </div>
+      </article>
+      <article className="details">
+        <div className="details__address-cont">
+          <h6 className="details__subhead">WAREHOUSE ADDRESS:</h6>
+          <p className="details__value">469 King Street West, Toronto, CAN</p>
+        </div>
+        <div className="details__contact-cont">
+          <div className="details__name-cont">
+            <h6 className="details__subhead">CONTACT NAME:</h6>
+            <p className="details__value">Graeme Lyon</p>
+            <p className="details__value">Warehouse Manager</p>
+          </div>
+          <div className="details__info-cont">
+            <h6 className="details__subhead">CONTACT INFORMATION:</h6>
+            <p className="details__value">+1 (951)384-7528</p>
+            <p className="details__value">email@email.com</p>
+          </div>
         </div>
       </article>
     </section>
--- client/src/pages/Warehouses/Warehouses.js
+++ client/src/pages/Warehouses/Warehouses.js
@@ -5,12 +5,12 @@ export default class Warehouses extends Component {
   render() {
     return (
       <main>
-        <div className="warehouse__heading">
+        {/* <div className="warehouse__heading">
           <figure className="warehouse__backarrow"></figure>
           <div className="warehouse__title"></div>
           <button className="warehouse__edit-button"></button>
-        </div>
-        <WarehouseDetails />;
+        </div> */}
+        <WarehouseDetails />
       </main>
     );
   }
