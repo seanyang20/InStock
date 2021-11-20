0a6e7ffc307627d879eb15fac3a8184422f4d31b
Hannah Park <hannahp056@gmail.com>
2021-11-20 18:26:44 -0500

    start mobile styles

--- client/src/pages/AddInventory/AddInventory.js
+++ client/src/pages/AddInventory/AddInventory.js
@@ -9,31 +9,33 @@ export default function AddInventory() {
         <img onClick="" src={arrow} alt="arrow" />
         <h1 className="add-inventory__title">Add New Inventory Item</h1>
       </section>
-      <section>
-        <h2>Item details</h2>
-        <label>Item Name</label>
-        <input></input>
-        <label>Description</label>
-        <textarea></textarea>
-        <label>Category</label>
-        <Select />
-      </section>
-      <section>
-        <h2>Item Availability</h2>
-        <h3>Status</h3>
-        <input type="radio"></input>
-        <label>In stock</label>
-        <input type="radio"></input>
-        <label>Out of Stock</label>
-        <label>Quantity</label>
-        <input type="number"></input>
-        <label>Warehouse</label>
-        <Select />
-      </section>
-      <section>
-        <button>Cancel</button>
-        <button>+ Add Item</button>
-      </section>
+      <form className="add-inventory__form">
+        <section className="add-inventory__form-section">
+          <h2 className="add-inventory__subtitle">Item details</h2>
+          <label className="add-inventory__label">Item Name</label>
+          <input className="add-inventory__details-name"></input>
+          <label className="add-inventory__label">Description</label>
+          <textarea className="add-inventory__details-description"></textarea>
+          <label className="add-inventory__label">Category</label>
+          <Select className="add-inventory__select" />
+        </section>
+        <section className="add-inventory__form-section">
+          <h2 className="add-inventory__subtitle">Item Availability</h2>
+          <h3 className="add-inventory__label">Status</h3>
+          <input type="radio" className="add-inventory__radio"></input>
+          <label className="add-inventory__radio-label">In stock</label>
+          <input type="radio" className="add-inventory__radio"></input>
+          <label className="add-inventory__radio-label">Out of Stock</label>
+          <label className="add-inventory__label">Quantity</label>
+          <input type="number" className="add-inventory__quantity"></input>
+          <label className="add-inventory__label">Warehouse</label>
+          <Select className="add-inventory__select" />
+        </section>
+        <section className="add-inventory__buttons">
+          <button className="add-inventory__cancel">Cancel</button>
+          <button className="add-inventory__add-button">+ Add Item</button>
+        </section>
+      </form>
     </article>
   );
 }
