81185f70659fd444a8e28115ba3159b5684b0d17
Sean Yang <seanyixuan@gmail.com>
2021-11-20 21:30:54 -0800

    got all the styling down

--- client/src/pages/EditInventory/EditInventory.js
+++ client/src/pages/EditInventory/EditInventory.js
@@ -21,40 +21,51 @@ export default function EditInventory(props) {
     }, [props.match.params.id])
 
   return (
-    <div className='editWarehouse'>
-    <form className='editWarehouse__container'>
-        <div className='editWarehouse__header'>
-            <span className='editWarehouse__arrow'/>
-            <h1 className='editWarehouse__title'>Edit Warehouse</h1>
+    <div className='editInventory'>
+    <form className='editInventory__container'>
+        <div className='editInventory__header'>
+            <span className='editInventory__arrow'/>
+            <h1 className='editInventory__title'>Edit Inventory</h1>
         </div>
-        <div className='editWarehouse__sections'>
-            <div className='editWarehouse__details'>
-                <h2 className='editWarehouse__details-title'>Warehouse Details</h2>
-                <p className='editWarehouse__input-title'>Warehouse Name</p>
-                <input name='warehouseName' className='editWarehouse__input' />
-                <p className='editWarehouse__input-title'>Street Address</p>
-                <input name='warehouseAddress' className='editWarehouse__input' />
-                <p className='editWarehouse__input-title'>City</p>
-                <input name='warehouseCity' className='editWarehouse__input'/>
-                <p className='editWarehouse__input-title'>Country</p>
-                <input name='warehouseCountry' className='editWarehouse__input' />
+        <div className='editInventory__sections'>
+            <div className='editInventory__details'>
+                <h2 className='editInventory__details-title'>Item Details</h2>
+                <p className='editInventory__input-title'>Item Name</p>
+                <input name='warehouseName' className='editInventory__input' />
+                <p className='editInventory__input-title'>Description</p>
+                <input name='warehouseAddress' className='editInventory__input editInventory__description' />
+                <p className='editInventory__input-title'>Category</p>
+                {/* <input name='warehouseCountry' className='editInventory__input' /> */}
+                <select name="warehouseCountry" className='editInventory__input'>
+                    {/* <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option> */}
+                </select>
             </div>
-            <div className='editWarehouse__sections-divider' ></div>
-            <div className='editWarehouse__details'>
-                <h2 className='editWarehouse__details-title'>Contact Details</h2>
-                <p className='editWarehouse__input-title'>Contact Name</p>
-                <input name='contactName' className='editWarehouse__input' />
-                <p className='editWarehouse__input-title'>Position</p>
-                <input name='contactPosition' className='editWarehouse__input' />
-                <p className='editWarehouse__input-title'>Phone Number</p>
-                <input name='contactPhone' className='editWarehouse__input' />
-                <p className='editWarehouse__input-title'>Email</p>
-                <input name='contactEmail' className='editWarehouse__input' />
+            <div className='editInventory__sections-divider' ></div>
+            <div className='editInventory__availability'>
+                <h2 className='editInventory__details-title'>Item Availability</h2>
+                <p className='editInventory__input-title'>Status</p>
+                <div name='contactName' className='editInventory__status' >
+                  <input type="radio" id="html" name="fav_language" value="HTML"/>
+                  <label htmlFor="html">In stock</label>
+                  <input type="radio" id="css" name="fav_language" value="CSS"/>
+                  <label htmlFor="css">Out of stock</label>
+                </div>
+                <p className='editInventory__input-title'>Warehouse</p>
+                {/* <input name='contactPosition' className='editInventory__input' /> */}
+                <select name='contactPosition' className='editInventory__input'>
+                    {/* <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option> */}
+                </select>
             </div>
         </div>
-        <div className='editWarehouse__actions'>
-            <button className='editWarehouse__actions--cancel'>Cancel</button>
-            <button type='submit' className='editWarehouse__actions--save'>Save</button>
+        <div className='editInventory__actions'>
+            <button className='editInventory__actions--cancel'>Cancel</button>
+            <button type='submit' className='editInventory__actions--save'>Save</button>
         </div>
     </form>
 </div>
