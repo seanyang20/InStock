54cd7845ac17c4c608376864139c8767b0bcaaeb
Sean Yang <seanyixuan@gmail.com>
2021-11-20 16:52:51 -0800

    using editwarehouse page as a template and got contents to display

--- client/src/pages/EditInventory/EditInventory.js
+++ client/src/pages/EditInventory/EditInventory.js
@@ -21,10 +21,42 @@ export default function EditInventory(props) {
     }, [props.match.params.id])
 
   return (
-    <div>
-        <p>
-     EDIT
-     </p>
-    </div>
+    <div className='editWarehouse'>
+    <form className='editWarehouse__container'>
+        <div className='editWarehouse__header'>
+            <span className='editWarehouse__arrow'/>
+            <h1 className='editWarehouse__title'>Edit Warehouse</h1>
+        </div>
+        <div className='editWarehouse__sections'>
+            <div className='editWarehouse__details'>
+                <h2 className='editWarehouse__details-title'>Warehouse Details</h2>
+                <p className='editWarehouse__input-title'>Warehouse Name</p>
+                <input name='warehouseName' className='editWarehouse__input' />
+                <p className='editWarehouse__input-title'>Street Address</p>
+                <input name='warehouseAddress' className='editWarehouse__input' />
+                <p className='editWarehouse__input-title'>City</p>
+                <input name='warehouseCity' className='editWarehouse__input'/>
+                <p className='editWarehouse__input-title'>Country</p>
+                <input name='warehouseCountry' className='editWarehouse__input' />
+            </div>
+            <div className='editWarehouse__sections-divider' ></div>
+            <div className='editWarehouse__details'>
+                <h2 className='editWarehouse__details-title'>Contact Details</h2>
+                <p className='editWarehouse__input-title'>Contact Name</p>
+                <input name='contactName' className='editWarehouse__input' />
+                <p className='editWarehouse__input-title'>Position</p>
+                <input name='contactPosition' className='editWarehouse__input' />
+                <p className='editWarehouse__input-title'>Phone Number</p>
+                <input name='contactPhone' className='editWarehouse__input' />
+                <p className='editWarehouse__input-title'>Email</p>
+                <input name='contactEmail' className='editWarehouse__input' />
+            </div>
+        </div>
+        <div className='editWarehouse__actions'>
+            <button className='editWarehouse__actions--cancel'>Cancel</button>
+            <button type='submit' className='editWarehouse__actions--save'>Save</button>
+        </div>
+    </form>
+</div>
   );
 }
