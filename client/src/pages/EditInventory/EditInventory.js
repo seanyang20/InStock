c16b6598c7a68ab65b7d3cb7f7d7264dd597b4b6
Sean Yang <seanyixuan@gmail.com>
2021-11-20 22:33:34 -0800

    got back arrow button to work & added handleSubmit with a not yet working form validation

--- client/src/pages/EditInventory/EditInventory.js
+++ client/src/pages/EditInventory/EditInventory.js
@@ -7,7 +7,14 @@ const apiUrl = 'http://localhost:8080'
 // const inventory_API_URL = "http://localhost:8080/inventories";
 
 export default function EditInventory(props) {
-    const [inventory, setInventory] = useState({contact: {}})
+    const [inventory, setInventory] = useState()
+    const [validation, setValidation] = useState({
+        warehouseName: true,
+        itemName: true,
+        description: true,
+        category: true,
+        status: true,
+      });
     const history = useHistory()
 
     useEffect(() => {
@@ -20,23 +27,85 @@ export default function EditInventory(props) {
         })
     }, [props.match.params.id])
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("inside handleSubmit for edit Inventory");
+       
+        const updatedInventory = {
+            warehouseName: event.target.warehouseName.value,
+            itemName: event.target.itemName.value,
+            description: event.target.description.value,
+            category: event.target.category.value,
+            // status: event.target.status.value,
+ 
+        }
+        console.log(inventory.id);
+        console.log(updatedInventory);
+
+        const warehouseNameIsValid = updatedInventory.warehouseName.value.length > 0;
+        const itemNameIsValid = updatedInventory.itemName.value.length > 0;
+        const descriptionIsValid = updatedInventory.description.value.length > 0;
+        const categoryIsValid = updatedInventory.category.value.length > 0;
+        // const statusIsValid = status.value.length > 0;
+
+        setValidation({
+            warehouseName: warehouseNameIsValid,
+            itemName: itemNameIsValid,
+            description: descriptionIsValid,
+            category: categoryIsValid,
+            // status: statusIsValid,
+          });
+
+          if (
+            !warehouseNameIsValid ||
+            !itemNameIsValid ||
+            !descriptionIsValid ||
+            !categoryIsValid 
+            // !statusIsValid 
+          ) {
+            alert("Please fill in the empty fields");
+            return;
+          }
+      
+        //   const newWarehouse = {
+        //     name: warehouseName.value,
+        //     address: streetAddress.value,
+        //     city: city.value,
+        //     country: country.value,
+        //     contactName: contactName.value,
+        //     position: position.value,
+        //     phone: phoneNumber.value,
+        //     email: email.value,
+        //   };
+    
+
+        axios.put(`${apiUrl}/inventories/${inventory.id}`, updatedInventory)
+        .then((response) => {
+            history.push('/')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+    console.log(validation);
+
   return (
     <div className='editInventory'>
-    <form className='editInventory__container'>
+    <form onSubmit={handleSubmit} className='editInventory__container'>
         <div className='editInventory__header'>
-            <span className='editInventory__arrow'/>
-            <h1 className='editInventory__title'>Edit Inventory</h1>
+            <span onClick={() => {history.push('/inventories')}} className='editInventory__arrow'/>
+            <h1 className='editInventory__title'>Edit Inventory Item</h1>
         </div>
         <div className='editInventory__sections'>
             <div className='editInventory__details'>
                 <h2 className='editInventory__details-title'>Item Details</h2>
                 <p className='editInventory__input-title'>Item Name</p>
-                <input name='warehouseName' className='editInventory__input' />
+                <input name='itemName' className='editInventory__input' />
                 <p className='editInventory__input-title'>Description</p>
-                <input name='warehouseAddress' className='editInventory__input editInventory__description' />
+                <input name='description' className='editInventory__input editInventory__description' />
                 <p className='editInventory__input-title'>Category</p>
                 {/* <input name='warehouseCountry' className='editInventory__input' /> */}
-                <select name="warehouseCountry" className='editInventory__input'>
+                <select name="category" className='editInventory__input'>
                     {/* <option value="volvo">Volvo</option>
                     <option value="saab">Saab</option>
                     <option value="mercedes">Mercedes</option>
@@ -47,15 +116,15 @@ export default function EditInventory(props) {
             <div className='editInventory__availability'>
                 <h2 className='editInventory__details-title'>Item Availability</h2>
                 <p className='editInventory__input-title'>Status</p>
-                <div name='contactName' className='editInventory__status' >
-                  <input type="radio" id="html" name="fav_language" value="HTML"/>
+                <div name='status' className='editInventory__status' >
+                  <input type="radio" id="inStock" name="fav_language" value="In stock"/>
                   <label htmlFor="html">In stock</label>
-                  <input type="radio" id="css" name="fav_language" value="CSS"/>
+                  <input type="radio" id="css" name="fav_language" value="Out of stock"/>
                   <label htmlFor="css">Out of stock</label>
                 </div>
                 <p className='editInventory__input-title'>Warehouse</p>
                 {/* <input name='contactPosition' className='editInventory__input' /> */}
-                <select name='contactPosition' className='editInventory__input'>
+                <select name='warehouseName' className='editInventory__input'>
                     {/* <option value="volvo">Volvo</option>
                     <option value="saab">Saab</option>
                     <option value="mercedes">Mercedes</option>
