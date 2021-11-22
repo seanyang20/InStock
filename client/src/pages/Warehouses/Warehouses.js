70c63d02739dafa6d742f7f8c3bcdbdf32a4bdb1
Joshua Young <prgrssn@gmail.com>
2021-11-22 12:52:48 -0800

    address other merge conflicts to allow for rendering

--- client/src/pages/Warehouses/Warehouses.js
+++ client/src/pages/Warehouses/Warehouses.js
@@ -1,52 +1,4 @@
 import "./Warehouses.scss";
-<<<<<<< HEAD
-import axios from "axios";
-import arrow from "../../assets/Icons/arrow_back-24px.svg";
-import error from "../../assets/Icons/error-24px.svg";
-import { useEffect, useState } from "react";
-import SelectedWarehouse from "../SelectedWarehouse/SelectedWarehouse";
-
-const apiUrl = "http://localhost:8080";
-
-export default function Warehouse(props) {
-    const [selectedWarehouse, setselectedWarehouse] = useState([]);
-    
-
-    useEffect(() => {
-        // axios.get(`${apiUrl}/warehouses`).then((res) => {
-        //     console.log(res.data);
-        //     // props.history.push("/warehouses");
-        
-        // });
-      
-        // axios.get(`${apiUrl}/warehouses/2922c286-16cd-4d43-ab98-c79f698aeab0`).then((res) => {
-        //     console.log(res.data);
-        //     // props.history.push("/warehouses");
-         
-        // });
-
-        axios.get(`${apiUrl}/warehouses/2922c286-16cd-4d43-ab98-c79f698aeab0/inventories`).then((res) => {
-            console.log(res.data);
-            // props.history.push("/warehouses");
-            setselectedWarehouse(res.data);
-        });
-      }, []);
-
-
-  
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    // props.history.push("/warehouses");
-  };
-
-  if (!selectedWarehouse) return null;
-
-  return (
-    <>
-    <SelectedWarehouse inventoryList={selectedWarehouse}/>
-    </>
-=======
 import WarehouseListItem from "../../components/WarehouseListItem/WarehouseListItem";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -123,6 +75,5 @@ export default function Warehouses(props) {
         </Modal>
       </div>
     </div>
->>>>>>> develop
   );
 }
