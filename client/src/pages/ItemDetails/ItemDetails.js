22e3df72160a5aa2a81c609aba96113cee8c7bc5
Hannah Park <hannahp056@gmail.com>
2021-11-22 16:39:33 -0500

    add responsive styling and edit button link

--- client/src/pages/ItemDetails/ItemDetails.js
+++ client/src/pages/ItemDetails/ItemDetails.js
@@ -3,6 +3,7 @@ import arrow from "../../assets/icons/arrow_back-24px.svg";
 import edit from "../../assets/icons/edit-24px.svg";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const apiUrl = "http://localhost:8080";
 
@@ -13,7 +14,6 @@ export default function ItemDetails(props) {
   useEffect(() => {
     axios.get(`${apiUrl}/inventories/${warehouseID}`).then((res) => {
       setInventoryItem(res.data);
-      console.log(inventoryItem);
     });
   }, []);
 
@@ -31,35 +31,48 @@ export default function ItemDetails(props) {
           <img src={arrow} alt="arrow icon" />
           <h1 className="item-details__title">{inventoryItem.itemName}</h1>
         </div>
-        <img src={edit} alt="pencil in a circle" />
+        <Link to={`/inventories/edit/${inventoryItem.id}`}>
+          <div className="item-details__edit">
+            <img
+              className="item-details__edit-icon"
+              src={edit}
+              alt="pencil in a circle"
+            />
+            <p className="item-details__edit-text">Edit</p>
+          </div>
+        </Link>
       </header>
       <article className="item-details__info-container">
         <section className="item-details__top-section">
           <div>
             <label className="item-details__label">ITEM DESCRIPTION:</label>
-            <p>{inventoryItem.description}</p>
+            <p className="item-details__text">{inventoryItem.description}</p>
           </div>
           <div>
             <label className="item-details__label">CATEGORY:</label>
             <p className="item-details__text">Electronics</p>
           </div>
         </section>
-        <section className="item-details__middle-section">
-          <div>
-            <label className="item-details__label">STATUS:</label>
-            <p className={statusClass}>{inventoryItem.status}</p>
-          </div>
-          <div>
-            <label className="item-details__label">QUANTITY:</label>
-            <p className="item-details__text">{inventoryItem.quantity}</p>
-          </div>
-        </section>
-        <section className="item-details__bottom-section">
-          <div>
-            <label className="item-details__label">WAREHOUSE:</label>
-            <p className="item-details__text">{inventoryItem.warehouseName}</p>
-          </div>
-        </section>
+        <div className="item-details__flex-right">
+          <section className="item-details__middle-section">
+            <div className="item-details__status">
+              <label className="item-details__label">STATUS:</label>
+              <p className={statusClass}>{inventoryItem.status}</p>
+            </div>
+            <div className="item-details__quantity">
+              <label className="item-details__label">QUANTITY:</label>
+              <p className="item-details__text">{inventoryItem.quantity}</p>
+            </div>
+          </section>
+          <section className="item-details__bottom-section">
+            <div>
+              <label className="item-details__label">WAREHOUSE:</label>
+              <p className="item-details__text">
+                {inventoryItem.warehouseName}
+              </p>
+            </div>
+          </section>
+        </div>
       </article>
     </section>
   );
