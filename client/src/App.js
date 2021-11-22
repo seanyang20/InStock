b5c587373485495799433b172d37c408c90ce1e1
Hannah Park <hannahp056@gmail.com>
2021-11-22 09:57:45 -0500

    pull data for item from back end endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
             }}
           />
           <Route
-            path="/inventories/details"
+            path="/inventories/:id"
             render={(props) => {
               return <ItemDetails {...props} />;
             }}
--- client/src/pages/ItemDetails/ItemDetails.js
+++ client/src/pages/ItemDetails/ItemDetails.js
@@ -1,8 +1,22 @@
 import "./ItemDetails.scss";
 import arrow from "../../assets/icons/arrow_back-24px.svg";
 import edit from "../../assets/icons/edit-24px.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function ItemDetails() {
+const apiUrl = "http://localhost:8080";
+
+export default function ItemDetails(props) {
+  const [inventoryItem, setInventoryItem] = useState([]);
+  const warehouseID = props.match.params.id;
+
+  useEffect(() => {
+    axios.get(`${apiUrl}/inventories/${warehouseID}`).then((res) => {
+      setInventoryItem(res.data);
+      console.log(inventoryItem);
+    });
+  }, []);
+
   // let statusClass = "item-details__value";
   // if (item.status === "In Stock") {
   //   statusClass = "item-details__value item-card__details--instock";
@@ -14,30 +28,30 @@ export default function ItemDetails() {
     <section>
       <header>
         <img src={arrow} alt="arrow icon" />
-        <h1>Television</h1>
+        <h1>{inventoryItem.itemName}</h1>
         {/* info needs to load in from data, doing basic markup */}
         <img src={edit} alt="pencil in a circle" />
       </header>
       <section>
         <label>ITEM DESCRIPTION:</label>
-        <p>This is the description of this item! it is a product we sell</p>
+        <p>{inventoryItem.description}</p>
         <label>CATEGORY:</label>
         <p>Electronics</p>
       </section>
       <section>
         <div>
           <label>STATUS:</label>
-          <p className="item-details__value">Electronics</p>
+          <p className="item-details__value">{inventoryItem.status}</p>
         </div>
         <div>
           <label>QUANTITY:</label>
-          <p>700</p>
+          <p>{inventoryItem.quantity}</p>
         </div>
       </section>
       <section>
         <div>
           <label>WAREHOUSE:</label>
-          <p>Manhattan</p>
+          <p>{inventoryItem.warehouseName}</p>
         </div>
       </section>
     </section>
