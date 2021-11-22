2a1185984aa7beca8f1d7c3710beec8990bb09ac
Matthew Maclean <matthew.maclean23@gmail.com>
2021-11-21 23:06:48 -0500

    Added DeleteInventoryModal component, added InventoryList functionality. changed opacity in deletemodal.scss

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -1,7 +1,40 @@
 import "./InventoryList.scss";
 import ItemCard from "../ItemCard/ItemCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../../components/DeleteInventoryModal/DeleteInventoryModal";
+const inventory_API_URL = "http://localhost:8080/inventories";
 
 export default function InventoryList({ inventory }) {
+  // inventory modal delete function
+  const [show, setShow] = useState(false);
+  // const [inventoryItem, setInventoryItem] = useState({});
+  const showModal = (id) => {
+    const modalInventory = inventory.find((inventoryItem) => {
+      return inventory.id === inventoryItem.id;
+    });
+
+    // setInventory(modalInventory);
+    console.log(modalInventory);
+    setShow(true);
+  };
+
+  const hideModal = () => {
+    setShow(false);
+  };
+
+  const handleDelete = () => {
+    console.log(inventory);
+    axios
+      .delete(`${inventory_API_URL}/:inventoryId`, inventory)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <section className="inventory-list">
       <article className="inventory-list__head">
@@ -17,8 +50,16 @@ export default function InventoryList({ inventory }) {
       </article>
       <article>
         {inventory.map((item) => (
-          <ItemCard key={item.id} item={item} />
+          <ItemCard key={item.id} item={item} showModal={showModal} />
         ))}
+        <Modal
+          show={show}
+          handleClose={hideModal}
+          inventory={inventory}
+          handleDelete={handleDelete}
+        >
+          <p>Modal</p>
+        </Modal>
       </article>
     </section>
   );
--- client/src/pages/Inventory/Inventory.js
+++ client/src/pages/Inventory/Inventory.js
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import InventoryList from "../../components/InventoryList/InventoryList";
+import Modal from "../../components/DeleteModal/DeleteModal";
 
 const inventory_API_URL = "http://localhost:8080/inventories";
 
 export default function Inventory() {
   const [inventoryList, setInventoryList] = useState([]);
+  // inventory modal delete function
+  const [show, setShow] = useState(false);
+
+  const [inventory, setInventory] = useState({});
+  const showModal = (id) => {
+    const modalInventory = inventoryList.find((inventory) => {
+      return inventory.id === id;
+    });
+
+    setInventory(modalInventory);
+    console.log(inventory);
+    setShow(true);
+  };
+
+  const hideModal = () => {
+    setShow(false);
+  };
+
+  const handleDelete = () => {
+    console.log(inventory);
+    axios
+      .delete(`${inventory_API_URL}/:inventoryId`, inventory)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     axios.get(inventory_API_URL).then((res) => {
@@ -17,7 +47,15 @@ export default function Inventory() {
 
   return (
     <>
-      <InventoryList inventory={inventoryList} />
+      <InventoryList inventory={inventoryList} showModal={showModal} />
+      <Modal
+        show={show}
+        handleClose={hideModal}
+        inventory={inventory}
+        handleDelete={handleDelete}
+      >
+        <p>Modal</p>
+      </Modal>
     </>
   );
 }
